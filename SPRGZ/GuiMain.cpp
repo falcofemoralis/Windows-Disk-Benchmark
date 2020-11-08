0d506ba11fce39e60d9903946141e01acb6c10e5
Falcofemoralis <araraunapappagallo@gmail.com>
2020-11-08 21:15:22 +0200

    Improved GUI

--- SPRGZ/GuiMain.cpp
+++ SPRGZ/GuiMain.cpp
@@ -7,7 +7,7 @@
 #define cb_list_disks_id 1
 #define cb_list_buffers_id 2
 #define cb_list_testCounts_id 3
-#define cb_list_types_id 4
+#define cb_list_modes_id 4
 #define btn_stop_id 5
 #define btn_pause_id 6
 #define btn_start_id 7
@@ -18,7 +18,7 @@
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam);
 
-const char* types[] = { "WRITE_THROUGH", "RANDOM_ACCESS", "SEQUENTIAL", "NO_BUFFERING"};
+const char* modes[] = { "WRITE_THROUGH", "RANDOM_ACCESS", "SEQUENTIAL", "NO_BUFFERING"};
 const char* buffNames[] = { "1 KB", "4 KB", "8 KB", "1 MB", "2 MB", "4 MB", "8 MB", "16 MB" };
 int buffSizes[] = { 1 * KB, 4 * KB, 8 * KB, 1 * MB, 2 * MB, 4 * MB, 8 * MB, 16 * MB };
 const char* disks[] = { "Диск А", "Диск Б" };
@@ -26,12 +26,11 @@ const char* testCounts[] = { "1", "2", "3", "4", "5" };
 
 struct Config {
 	int bufferSize;
-	const char* type;
+	const char* mode;
 	const char* diskPath;
 };
 
 Config userConfig;
-
 
 int main() {
 	WNDCLASS wcl;
@@ -54,6 +53,12 @@ int main() {
 	return 0;
 }
 
+void setFont(HWND hwnd, int size, int weight) {
+	HFONT font = CreateFont(size, 0, 0, 0, weight, FALSE, FALSE, FALSE, ANSI_CHARSET,
+		OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS, PROOF_QUALITY, DEFAULT_PITCH | FF_SWISS, "Arial");
+	SendMessage(hwnd, WM_SETFONT, WPARAM(font), TRUE);
+}
+
 // Структура с параметрами view объектов: x,y - координаты объекта, width,height - размеры view
 struct ViewParam {
 	int x, y;
@@ -68,14 +73,19 @@ void createCombobox(const char* nameBox, ViewParam* params, const char* values[]
 	for (int i = 0; i < countValues; ++i)
 		SendMessage(dropList, CB_ADDSTRING, 0, LPARAM(values[i]));
 	SendMessage(dropList, CB_SETCURSEL, 0, 0);
+	setFont(dropList, 16, FW_MEDIUM);
 }
 
 void createButton(const char* nameBtn, ViewParam* params, int id, HWND& hwnd) {
-	HWND btn_start = CreateWindow("button", nameBtn, WS_VISIBLE | WS_CHILD, params->x, params->y, params->width, params->height, hwnd, (HMENU)id, NULL, NULL);
+	HWND btn;
+	btn = CreateWindow("button", nameBtn, WS_VISIBLE | WS_CHILD, params->x, params->y, params->width, params->height, hwnd, (HMENU)id, NULL, NULL);
+	setFont(btn, 17, FW_BOLD);
 }
 
-void createText(const char* nameBtn, ViewParam* params, int id, HWND& hwnd) {
-	HWND btn_start = CreateWindow("static", nameBtn, WS_VISIBLE | WS_CHILD | SS_CENTER, params->x, params->y, params->width, params->height, hwnd, (HMENU)id, NULL, NULL);
+void createText(const char* nameBtn, ViewParam* params, int id, HWND& hwnd, int isBold, int size) {
+	HWND stc;
+	stc = CreateWindow("static", nameBtn, WS_VISIBLE | WS_CHILD | SS_CENTER, params->x, params->y, params->width, params->height, hwnd, (HMENU)id, NULL, NULL);
+	setFont(stc, size, isBold);
 }
 
 void createProgressBar(ViewParam* params, int id, HWND& hwnd) {
@@ -100,7 +110,7 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) 
 		createCombobox("Диски", new ViewParam{ 10, 10, 180, 400 }, disks, sizeof(disks) / sizeof(disks[0]), cb_list_disks_id, hwnd);
 		createCombobox("Кол-во тестов", new ViewParam{ 200, 10, 50, 500 }, testCounts, sizeof(testCounts) / sizeof(testCounts[0]), cb_list_testCounts_id, hwnd);
 		createCombobox("Размер буфера", new ViewParam{ 260, 10, 100, 500 }, buffNames, sizeof(buffNames) / sizeof(buffNames[0]), cb_list_buffers_id, hwnd);
-		createCombobox("Типы", new ViewParam{ 370, 10, 150, 500 }, types, sizeof(types) / sizeof(types[0]), cb_list_types_id, hwnd);
+		createCombobox("Типы", new ViewParam{ 370, 10, 150, 500 }, modes, sizeof(modes) / sizeof(modes[0]), cb_list_modes_id, hwnd);
 
 		createButton("Старт", new ViewParam{ 400, 40, 120, 30 }, btn_start_id, hwnd);
 		createButton("Пауза", new ViewParam{ 400, 80, 120, 30 }, btn_pause_id, hwnd);
@@ -111,13 +121,13 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) 
 		createButton("Стоп", new ViewParam{ 400, 120, 120, 30 }, btn_stop_id, hwnd);
 
 
-		createText("Чтение", new ViewParam{ 50, 60, 100, 20 }, NULL, hwnd);
-		createText("Запись", new ViewParam{ 250, 60, 100, 20 }, NULL, hwnd);
+		createText("Чтение", new ViewParam{ 50, 60, 100, 20 }, NULL, hwnd, FW_HEAVY, 25);
+		createText("Запись", new ViewParam{ 250, 60, 100, 20 }, NULL, hwnd, FW_HEAVY, 25);
 
-		createText("130 MB/s", new ViewParam{ 50, 100, 100, 20 }, NULL, hwnd);
-		createText("250 MB/s", new ViewParam{ 250, 100, 100, 20 }, NULL, hwnd);
+		createText("130 MB/s", new ViewParam{ 50, 100, 100, 20 }, NULL, hwnd, FW_MEDIUM, 18);
+		createText("250 MB/s", new ViewParam{ 250, 100, 100, 20 }, NULL, hwnd, FW_MEDIUM, 18);
 
-		createProgressBar(new ViewParam{ 10, 160, 510, 30 }, NULL, hwnd);
+		createProgressBar(new ViewParam{ 10, 250, 510, 30 }, NULL, hwnd);
 		break;
 	case WM_COMMAND:
 
@@ -126,8 +136,8 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) 
 			int selectedId = SendMessage(HWND(lParam), CB_GETCURSEL, 0, 0);
 			switch (LOWORD(wParam))
 			{
-			case cb_list_types_id:
-				userConfig.type = types[selectedId];
+			case cb_list_modes_id:
+				userConfig.mode = modes[selectedId];
 				break;
 			case cb_list_buffers_id:
 				userConfig.bufferSize = buffSizes[selectedId];
