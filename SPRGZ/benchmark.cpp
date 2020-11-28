217362a76736af82590d7d04bd8ee93e2c282458
Falcofemoralis <araraunapappagallo@gmail.com>
2020-11-28 18:00:30 +0200

    Fixed bug

--- SPRGZ/benchmark.cpp
+++ SPRGZ/benchmark.cpp
@@ -257,8 +257,10 @@ VOID saveResults(DOUBLE* valuesArray, TCHAR* fileName, DWORD size, DWORD type) {
     if (INVALID_HANDLE_VALUE == hFile) return;
     
     if (type == TYPE_GRAPH) {
-        for (DWORD i = 1; i < size; ++i) {
-            sprintf(buffer, "%d;%.6lf\n\0", i * testConfig.bufferSize, valuesArray[i - 1]);
+        DOUBLE totalTime = 0;
+        for (DWORD i = 1; i <= size; ++i) {
+            totalTime += valuesArray[i - 1];
+            sprintf(buffer, "%d;%.3lf\n\0", i * testConfig.bufferSize, totalTime);
             WriteFile(hFile, buffer, _tcslen(buffer) * sizeof(TCHAR), &dwTemp, NULL);
         }
     }
@@ -291,6 +293,8 @@ VOID saveResults(DOUBLE* valuesArray, TCHAR* fileName, DWORD size, DWORD type) {
                 }
         
         for (DWORD i = 0; i < MAX_ITERATIONS; ++i) {
+            if (iterations[i] == 0)
+                continue;
             sprintf(buffer, "%3.2lf;%.3lf-%.3lf\n\0", ((DOUBLE)iterations[i] / size) * 100, ranges[i], ranges[i + 1]);
             WriteFile(hFile, buffer, _tcslen(buffer) * sizeof(TCHAR), &dwTemp, NULL);
         }
